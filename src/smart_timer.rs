c61704cd451156fb92a495c1b5a3ec437bcf1ac6
p3zz <federico.pezzato.work@gmail.com>
2022-05-23 18:56:48 +0200

    add smart timer

--- src/smart_timer.rs
+++ src/smart_timer.rs
@@ -2,7 +2,7 @@ use stm32h7xx_hal::device::DWT;
 use stm32h7xx_hal::time::Hertz;
 
 pub struct SmartTimer {
-    clock: Hertz,
+    pub clock: Hertz,
 }
 
 impl SmartTimer {
@@ -11,9 +11,19 @@ impl SmartTimer {
     }
 
     /// returns the time elapsed since the program started
-    /// unit: second
-    pub fn now(&self) -> f32 {
+    /// unit: us
+    pub fn now_s(&self) -> f32 {
         let cycles = DWT::get_cycle_count();
         cycles as f32 / self.clock.0 as f32
+    }
+
+    pub fn now_ms(&self) -> f32 {
+        let cycles = DWT::get_cycle_count();
+        (cycles as f32 / self.clock.0 as f32) * 1_000 as f32
+    }
+
+    pub fn now_us(&self) -> f32 {
+        let cycles = DWT::get_cycle_count();
+        (cycles as f32 / self.clock.0 as f32) * 1_000_000 as f32
     }
 }
