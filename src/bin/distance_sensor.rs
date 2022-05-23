8583b628702a73c05c36d36ec4f40ab8a08805ed
p3zz <federico.pezzato.work@gmail.com>
2022-05-23 18:57:26 +0200

    add hc-sr04 usage example

--- src/bin/distance_sensor.rs
+++ src/bin/distance_sensor.rs
@@ -6,17 +6,24 @@ use panic_halt as _;
 use stm32h7xx_hal::{prelude::*, timer::Timer, serial::{self, Serial, Error}};
 
 use cortex_m_rt::entry;
-use nucleo_h743zi::hc_sr04::HcSr04;
 
 use core::fmt::Write;
+use cortex_m::asm;
+use embedded_hal::digital::v2::OutputPin;
 use stm32h7xx_hal::time::{Hertz};
+
+use stm32h7xx_hal::device::DWT;
+
 use nucleo_h743zi::smart_timer::SmartTimer;
+use nucleo_h743zi::hc_sr04::HcSr04;
+use nucleo_h743zi::rusty_logger::RustyLogger;
 
 #[entry]
 fn main() -> ! {
     // Get access to the device specific peripherals from the peripheral access crate
     let dp = stm32h7xx_hal::stm32::Peripherals::take().unwrap();
-    let cp = cortex_m::Peripherals::take().unwrap();
+    let mut cp = cortex_m::Peripherals::take().unwrap();
+    cp.DWT.enable_cycle_counter();
     let core_clock: Hertz = 100_000_000.hz();
 
     // Take ownership over the RCC devices and convert them into the corresponding HAL structs
@@ -28,8 +35,7 @@ fn main() -> ! {
 
     // Freeze the configuration of all the clocks in the system and
     // retrieve the Core Clock Distribution and Reset (CCDR) object
-    let rcc = rcc.use_hse(core_clock).bypass_hse();
-    let ccdr = rcc.freeze(pwrcfg, &dp.SYSCFG);
+    let ccdr = rcc.sys_ck(core_clock).freeze(pwrcfg, &dp.SYSCFG);
 
     // Acquire the GPIOB peripheral
     let gpioc = dp.GPIOC.split(ccdr.peripheral.GPIOC);
@@ -43,8 +49,6 @@ fn main() -> ! {
     // Acquire the GPIOD peripheral
     let gpiod = dp.GPIOD.split(ccdr.peripheral.GPIOD);
 
-    let delay = cp.SYST.delay(ccdr.clocks);
-
     gpiod.pd8.into_alternate_af7();
     gpiod.pd9.into_alternate_af7();
 
@@ -56,22 +60,19 @@ fn main() -> ! {
         &ccdr.clocks,
     ).unwrap();
 
-    // retrieve rx and tx
-    let (mut tx, mut rx) = serial.split();
-
     // configure software timer
     let timer = SmartTimer::new(core_clock);
 
-    // configure hc-sr04
-    let mut hc_sr04 = HcSr04::new(trigger, echo, timer);
+    let mut logger = RustyLogger::new(serial);
 
+    // configure hc-sr04
+    let mut hc_sr04 = HcSr04::new(trigger, echo, &timer, logger);
 
     loop {
         match hc_sr04.wait_distance() {
-            Some(distance) => {
-                writeln!(tx, "Distance: {}", distance.mm()).unwrap();
-            }
+            Some(distance) => {}
             None => {}
         }
+
     }
 }
